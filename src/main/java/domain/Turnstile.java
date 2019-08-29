6aedfc73798a82ed5da333d77885c888faf402fb
stefanromanov <romanovsc2@gmail.com>
2019-08-29 22:31:35 +0300

    Added some comments. Stylistic changes.

--- src/main/java/domain/Turnstile.java
+++ src/main/java/domain/Turnstile.java
@@ -10,30 +10,36 @@ public class Turnstile {
 
     public boolean putCoin() {
 
+        //Inserting coin when in Paid and Open states not allowed.
         if(this.getState() == TurnstileState.Paid || this.getState() == TurnstileState.Open){
             return false;
         }
 
+        //Generating next state
         this.setState(this.getState().coin());
         return true;
     }
 
     public boolean canPass() {
 
+        //Passing through the gate when it is in status Locked is not allowed.
         if(this.getState() == TurnstileState.Locked){
             return false;
         }
 
+        //Generating next state
         this.setState(this.getState().pass());
         return true;
     }
 
     public void key() {
+
+        //Generating next state
         this.setState(this.state.key());
     }
 
     public TurnstileState getState() {
-        return state;
+        return this.state;
     }
 
     private void setState(TurnstileState newState) {
--- src/main/java/domain/TurnstileState.java
+++ src/main/java/domain/TurnstileState.java
@@ -4,48 +4,57 @@ public enum TurnstileState {
     Locked{
         @Override
         public TurnstileState coin(){
+            //State transition from Locked to Paid
             return TurnstileState.Paid;
         }
 
         @Override
         public TurnstileState pass(){
+            //State remains unchanged
             return this;
         }
 
         @Override
         public TurnstileState key(){
+            //State transition from Locked to Open
             return TurnstileState.Open;
         }
     },
     Paid{
         @Override
         public TurnstileState coin(){
+            //State remains unchanged
             return this;
         }
 
         @Override
         public TurnstileState pass(){
+            //State transition from Paid to Locked
             return TurnstileState.Locked;
         }
 
         @Override
         public TurnstileState key(){
+            //State transition from Paid to Open
             return TurnstileState.Open;
         }
     },
     Open{
         @Override
         public TurnstileState coin(){
+            //State remains unchanged
             return this;
         }
 
         @Override
         public TurnstileState pass(){
+            //State remains unchanged
             return this;
         }
 
         @Override
         public TurnstileState key(){
+            //State transition from Open to Locked
             return TurnstileState.Locked;
         }
     };
